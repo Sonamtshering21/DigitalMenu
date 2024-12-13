7c604a48908c82e837fc64c574f855a5c95dc7fb
sonam tshering <sonamtshering1213@gmail.com>
2024-12-13 22:02:00 +0530

    Updated middleware.js

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
 export { default } from "next-auth/middleware";
 export const config = { 
-    matcher: ["/dashboard","/admin","/qr-codes"] // Apply middleware to multiple routes
+    matcher: ["/dashboard/:path*","/admin/:path*","/qr-codes/:path*"] // Apply middleware to multiple routes
   };
